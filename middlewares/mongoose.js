78c54a053bcc4ce992e881a312edc12d822d8cd6
usmansiddique123 <usman.siddique@devsinc.com>
2023-04-08 04:28:32 +0500

    testing api's

--- middlewares/mongoose.js
+++ middlewares/mongoose.js
@@ -5,8 +5,6 @@ const connectDb = (handler) => async (req, res) => {
     console.log("Connecting")
     return handler(req, res)
   }
-  console.log(mongoose.connections[0].readyState)
-
   await mongoose.set('strictQuery', false)
   mongoose.connect('mongodb+srv://admin:admin@cluster0.jrjaw5z.mongodb.net/test', () => {
     console.log('Connected to MongoDB', process.env.MONGO_URL)
