8b00410c21b136ca7b5798bf39f2e8b0cfb7b3a4
usmansiddique123 <usman.siddique@devsinc.com>
2023-04-08 16:11:00 +0500

    logging mongo state and debugging

--- middlewares/mongoose.js
+++ middlewares/mongoose.js
@@ -1,12 +1,15 @@
 import mongoose from "mongoose"
 
 const connectDb = (handler) => async (req, res) => {
+    console.log('Connecting', mongoose.connections[0].readyState)
 
   if (mongoose.connections[0].readyState) {
+    console.log("Connected")
     return
   }
   await mongoose.set('strictQuery', true)
   await mongoose.connect('mongodb+srv://admin:admin@cluster0.jrjaw5z.mongodb.net/test')
+  console.log('Connected to MongoDB and returning handler')
 
 }
 
