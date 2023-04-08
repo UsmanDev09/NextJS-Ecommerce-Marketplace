decc3cd3f04b185e4f3aff4f8f28dbb262529e2d
usmansiddique123 <usman.siddique@devsinc.com>
2023-04-08 15:49:12 +0500

    logging mongo state and debugging

--- pages/api/getcategories.js
+++ pages/api/getcategories.js
@@ -3,7 +3,9 @@ import category from '../../model/category';
 
 const handler = async (req, res) => {
   if (req.method === 'GET') {
+    console.log('GET')
     let categories = await category.find().populate('products');
+    console.log('categories: ', categories)
     res.status(200).json(categories);
   } else {
     res.status(404).json({ success: 'error' });
