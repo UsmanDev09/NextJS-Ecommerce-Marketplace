60388f4d845732f7680e07beefc6fe7f69d12f47
usmansiddique123 <usman.siddique@devsinc.com>
2023-04-08 16:17:59 +0500

    logging mongo state and debugging

--- middlewares/mongoose.js
+++ middlewares/mongoose.js
@@ -5,12 +5,15 @@ const connectDb = (handler) => async (req, res) => {
 
   if (mongoose.connections[0].readyState) {
     console.log("Connected")
-    return
+    return handler(req, res)
   }
+  console.log("Connecting", mongoose.connections[0].readyState)
   await mongoose.set('strictQuery', true)
-  await mongoose.connect('mongodb+srv://admin:admin@cluster0.jrjaw5z.mongodb.net/test')
+  await mongoose.connect('mongodb+srv://admin:admin@cluster0.jrjaw5z.mongodb.net/test', () => {
+    console.log('Connected to MongoDB', process.env.MONGO_URL)
+  })
   console.log('Connected to MongoDB and returning handler')
-
+  return handler(req, res)
 }
 
 export default connectDb
--- pages/api/getcategories.js
+++ pages/api/getcategories.js
@@ -1,13 +1,15 @@
 import connectDb from '../../middlewares/mongoose';
 import category from '../../model/category';
-connectDb()
+
 const handler = async (req, res) => {
   if (req.method === 'GET') {
+    console.log('GET')
     let categories = await category.find().populate('products');
+    console.log('categories: ', categories)
     res.status(200).json(categories);
   } else {
     res.status(404).json({ success: 'error' });
   }
 };
 
-export default handler;
+export default connectDb(handler);
