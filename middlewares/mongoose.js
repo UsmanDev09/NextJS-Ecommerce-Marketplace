854cc0ef2318edfff1871d30b74a856abd2e0a6c
usmansiddique123 <usman.siddique@devsinc.com>
2023-04-09 22:19:38 +0500

    Mongoose timeout changed to 5 seconds

--- middlewares/mongoose.js
+++ middlewares/mongoose.js
@@ -9,9 +9,17 @@ const connectDb = (handler) => async (req, res) => {
   }
   console.log("Connecting", mongoose.connections[0].readyState)
   await mongoose.set('strictQuery', true)
-  await mongoose.connect('mongodb+srv://admin:admin@cluster0.jrjaw5z.mongodb.net/test', () => {
-    console.log('Connected to MongoDB', process.env.MONGO_URL)
-  })
+  await mongoose.connect(
+    'mongodb+srv://admin:admin@cluster0.jrjaw5z.mongodb.net/test',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000,
+    },
+    () => {
+      console.log('Connected to MongoDB', process.env.MONGO_URL)
+    },
+  )
   console.log('Connected to MongoDB and returning handler')
   return handler(req, res)
 }
